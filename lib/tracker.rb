24e935bac1a2f33d8d96927e33f1f9260e79ec55
Senthil Arivudainambi <senthil@walkerandcobrands.com>
2018-06-27 17:42:23 -0700

    require railtie

--- lib/tracker.rb
+++ lib/tracker.rb
@@ -4,6 +4,7 @@ require_relative "tracker/middleware"
 require_relative "tracker/controller"
 require_relative "tracker/handlers"
 require_relative "tracker/background"
+require_relative "tracker/railtie"
 
 module Tracker
   GoogleAnalytics = Handlers::GoogleAnalytics::Queuer
--- lib/tracker/railtie.rb
+++ lib/tracker/railtie.rb
@@ -1,5 +1,5 @@
 class Tracker::Railtie < ::Rails::Railtie
-  initializer "rack-tracker.configure_controller" do |app|
+  initializer "tracker.configure_controller" do |app|
     ActiveSupport.on_load :action_controller do
       include Tracker::Controller
     end
